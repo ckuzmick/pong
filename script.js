9c87fdbd81f399654a934c524d1cb0bed209d1ec
Conrad Kuzmick <33187769+ckuzmick@users.noreply.github.com>
2023-11-27 14:33:52 -0500

    here

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 const canvas = document.getElementById("canvas")
 const innerCanvas = document.getElementById("canvas").innerHTML
-
-canvas.style.width = '800px';
-canvas.style.height = "600px";
-canvas.style.backgroundColor = 'grey';
